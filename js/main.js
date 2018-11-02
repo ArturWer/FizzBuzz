6da0ad8d9a061e61b693ba960eb4ba92089a2bbe
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-11-02 06:16:17 +0200

    Realized creating buttons in main with results the loop.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 "use strict";
+let main = document.querySelector("main");
+function writeResults(string) {
+	let btn = document.createElement("button");
+	let content = document.createTextNode(string);
+	btn.appendChild(content);
+	main.appendChild(btn);
+}
+
 for (var i = 1; i <= 100; i++) {
-	if (  ((i%3)===0) && ((i%5)===0)  ) {
-		console.log("FizzBuzz");
+	if ( ((i%3)===0) && ((i%5)===0) ) {
+		writeResults("FizzBuzz");
 	} else if ((i%3)===0) {
-		console.log("Fizz");
+		writeResults("Fizz");
 	} else if ((i%5)===0) {
-		console.log("Buzz");
+		writeResults("Buzz");
 	} else {
-		console.log(i);
+		writeResults(i);
 	}
 }
 	
